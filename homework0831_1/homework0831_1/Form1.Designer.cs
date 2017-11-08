e5903c62a3dd938087660cb853f7e8ee8b901748
MVC <32860858+sses6121@users.noreply.github.com>
2017-11-08 20:58:02 +0800

    Add files via upload

---
+++ homework0831_1/homework0831_1/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace homework0831_1 {
+    partial class Form1 {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.OK = new System.Windows.Forms.Button();
+            this.ResultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "請輸入整數";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(21, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(249, 33);
+            this.textBox1.TabIndex = 1;
+            // 
+            // OK
+            // 
+            this.OK.Location = new System.Drawing.Point(324, 59);
+            this.OK.Name = "OK";
+            this.OK.Size = new System.Drawing.Size(127, 37);
+            this.OK.TabIndex = 2;
+            this.OK.Text = "確定";
+            this.OK.UseVisualStyleBackColor = true;
+            this.OK.Click += new System.EventHandler(this.OK_Click);
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.AutoSize = true;
+            this.ResultLabel.Location = new System.Drawing.Point(17, 120);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(58, 21);
+            this.ResultLabel.TabIndex = 3;
+            this.ResultLabel.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 169);
+            this.Controls.Add(this.ResultLabel);
+            this.Controls.Add(this.OK);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "Form1";
+            this.Text = "判斷數值為奇數OR偶數";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button OK;
+        private System.Windows.Forms.Label ResultLabel;
+    }
+}
+
---
+++ homework0831_1/homework0831_1/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace homework0831_1 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+            ResultLabel.Text = "請輸入數字後按確定鈕，"+System.Environment.NewLine+"" +
+                "電腦會判斷是奇數還是偶數!";
+        }
+        
+        private void OK_Click(object sender, EventArgs e) {
+            try {
+                int x= Convert.ToInt32(textBox1.Text);
+                switch(x%2) {
+                    case 0:
+                        ResultLabel.Text = x.ToString() + "是偶數";
+                        break;
+                    case 1:
+                        ResultLabel.Text = x.ToString() + "是奇數";
+                        break;
+                }
+            } 
+            catch {
+                MessageBox.Show("請輸入數字");
+            }
+        }
+    }
+}
