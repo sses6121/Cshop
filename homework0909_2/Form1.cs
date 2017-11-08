53f8206a149068afdd6bf6289747c0775c3f74b5
sses6121 <32860858+sses6121@users.noreply.github.com>
2017-11-08 20:42:52 +0800

    C# windowsForm

---
+++ homework0909_2/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test0909_2 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+        int [] img = new int[4] {1,2,3,4};
+        private void 第一張ToolStripMenuItem_Click(object sender, EventArgs e) {
+            pictureBox1.Image = Image.FromFile("img" + img[0] + ".jpg");
+            
+        }
+
+        private void 第二張ToolStripMenuItem_Click(object sender, EventArgs e) {
+            pictureBox1.Image = Image.FromFile("img" + img[1] + ".jpg");
+        }
+
+        private void 第三張ToolStripMenuItem_Click(object sender, EventArgs e) {
+            pictureBox1.Image = Image.FromFile("img" + img[2] + ".jpg");
+        }
+
+        private void 第四張ToolStripMenuItem_Click(object sender, EventArgs e) {
+            pictureBox1.Image = Image.FromFile("img" + img[3] + ".jpg");
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            pictureBox1.Image = Image.FromFile("img" + img[0] + ".jpg");
+        }
+    }
+}
