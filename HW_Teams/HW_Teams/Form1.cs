8de97e7201151d5b8f0e241bf11af268824c6b09
MVC <32860858+sses6121@users.noreply.github.com>
2017-11-09 20:54:54 +0800

    Add files via upload

--- HW_Teams/HW_Teams/Form1.cs
+++ HW_Teams/HW_Teams/Form1.cs
@@ -29,8 +29,9 @@ namespace HW_Teams {
         string engS = "";
         private void button1_Click(object sender, EventArgs e) {
             Encoding encoder = Encoding.GetEncoding("Big5");
-            StreamReader sr = new StreamReader(@"D:\temp\teams.txt", encoder);
+            StreamReader sr = new StreamReader(@"D:\Temp\teams.txt", encoder);
             string[] chineseString = new string[13];
+            int count = 0;
             while (!sr.EndOfStream) {
                 //try { 
                 string line = sr.ReadLine();
@@ -47,7 +48,7 @@ namespace HW_Teams {
                 char[] engchar = new char[lineByte.Length];
                 //byte[] chineseByte;
                 //byte[] engByte;
-                int count = 0;
+                
                 string aaatemp = "";
                 for (int i = 0; i < lineByte.Length; i += 1) {
 
@@ -55,9 +56,9 @@ namespace HW_Teams {
                         chineseByte[i] = lineByte[i];
                         chineseByte[i + 1] = lineByte[i + 1];
                         chineseByte[i + 2] = lineByte[i + 2];
-                        chinesestring[i] += encoder.GetString(chineseByte, i, 2);
+                        chinesestring[i] = encoder.GetString(chineseByte, i, 2);
                         i += 1;
-                        count += 2;
+                        count += 1;
                     } else if (lineByte[i] < 128) {
 
                         //engS = encoder.GetString(lineByte,count,)
@@ -69,74 +70,95 @@ namespace HW_Teams {
                         //MessageBox.Show(aaatemp);
                     }
 
-                    for (int j = 0; j < chinesestring.Length; j++) {
-                        if (chinesestring[j] != null)
-                            chineseS += chinesestring[j];
-                    }
-                }
 
-                textBox1.Text = chineseS.Substring(0, 3) + System.Environment.NewLine + chineseS.Substring(3, 4);
-                listBox1.Items.Add(chineseS.Substring(0, 3));
-                listBox1.Items.Add(chineseS.Substring(3, 4));
-                sr.Close();
+                    /* for (int j = 0; j < count; j++) {
+                         if (chinesestring[j] != null)
+                             chineseS += chinesestring[j];
+                     }
+                     listBox1.Items.Add(chineseS);
+                 }*/
+
+                   
+
+                }
+                for (int j = 0; j < chinesestring.Length; j++) {
+                    if (chinesestring[j] != null)
+                        chineseS += chinesestring[j];
+                }
             }
+            sr.Close();
+            textBox1.Text = chineseS.Substring(0, 3) + System.Environment.NewLine + chineseS.Substring(3,4);
+            listBox1.Items.Add(chineseS.Substring(0, 3));
+            listBox1.Items.Add(chineseS.Substring(3, 4));
         }
 
 
         private void button2_Click(object sender, EventArgs e) {
             Encoding encoder = Encoding.GetEncoding("Big5");
-            StreamReader sr = new StreamReader(@"D:\temp\teams.txt", encoder);
-
-            /*while (!sr.EndOfStream) {
+            StreamReader sr = new StreamReader(@"D:\Temp\teams.txt", encoder);
+            string[] chineseString = new string[13];
+            int count = 0;
+            string [] engString = new string[13];
+            while (!sr.EndOfStream) {
                 //try { 
                 string line = sr.ReadLine();
                 string temp = line;
+                int kk = 0;
                 line = temp.Replace(" ", "");
-                MessageBox.Show(line);
                 byte[] lineByte = encoder.GetBytes(line);
-                string[] engstring = new string[lineByte.Length];
                 string[] chinesestring = new string[lineByte.Length];
+                string[] engstring = new string[lineByte.Length];
+
                 byte[] chineseByte = new byte[lineByte.Length];
                 byte[] engByte = new byte[lineByte.Length];
                 char[] chinesechar = new char[lineByte.Length];
                 char[] engchar = new char[lineByte.Length];
                 //byte[] chineseByte;
                 //byte[] engByte;
-                int count = 0;
+
                 string aaatemp = "";
                 for (int i = 0; i < lineByte.Length; i += 1) {
 
                     if (lineByte[i] >= 128) {
                         chineseByte[i] = lineByte[i];
                         chineseByte[i + 1] = lineByte[i + 1];
-                        //chineseByte[i + 2] = lineByte[i + 2];
-                        if (chinesestring[i] != "") {
-                            chinesestring[i] += encoder.GetString(chineseByte, i, 2);
-                            //MessageBox.Show(encoder.GetString(chineseByte, i, 2));
-                            //chinesestring += chinesechar[i].ToString();
-                        }
+                        chineseByte[i + 2] = lineByte[i + 2];
+                        chinesestring[i] = encoder.GetString(chineseByte, i, 2);
                         i += 1;
-                        count += 2;
+                        count += 1;
+                    } else if (lineByte[i] < 128) {
 
-
-                    } else {
                         //engS = encoder.GetString(lineByte,count,)
                         engByte[i] = lineByte[i];
                         //engchar = Encoding.UTF8.GetChars(engByte, i, 1);
-                        string aaa = Encoding.UTF8.GetString(engByte, i, 1);
-                        aaatemp += aaa;
+                        engstring[i] = Encoding.UTF8.GetString(engByte, i, 1);
+                        //aaatemp += aaa;
                         //engstring +=
-                        MessageBox.Show(aaatemp);
+                        //MessageBox.Show(aaatemp);
+                        //engString[i] = aaatemp;
                     }
+
+                   // MessageBox.Show(aaatemp);
+                    /* for (int j = 0; j < count; j++) {
+                         if (chinesestring[j] != null)
+                             chineseS += chinesestring[j];
+                     }
+                     listBox1.Items.Add(chineseS);
+                 }*/
+
+
+
                 }
-
-                string s = "";
-                string temps = "";
-
-                listBox1.Items.Add(chineseS);
+                for (int j = 0; j < engstring.Length; j++) {
+                    if (engstring[j] != null)
+                        engS += engstring[j];
+                   // MessageBox.Show(engstring[j]);
+                }
             }
             sr.Close();
-        }*/
-    }
+            textBox1.Text = engS.Substring(0,3) +  System.Environment.NewLine + engS.Substring(3,3);
+            listBox1.Items.Add(engS.Substring(0, 3));
+            listBox1.Items.Add(engS.Substring(3, 3));
         }
     }
+        }
